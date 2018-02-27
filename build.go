4d8665df780a164feae374b547ba3e3e36f77f8d
soeyusuke <s1240231@gmail.com>
2018-02-28 00:37:42 +0900

    [ADD] Build func add and Fixed some bugs

--- build.go
+++ build.go
@@ -11,9 +11,19 @@ type MsgSetter interface {
 
 var TransactionID MsgSetter = transactionIDSetter{}
 
+func (m *Message) build(s ...MsgSetter) error {}
+
+// wraps m.build
+func Build(s ...MsgSetter) (*Message, error) {
+	m := new(Message)
+	return m, m.build(s...)
+}
+
 func MessageBuild(s ...MsgSetter) *Message {
 	m, err := Build(s...)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	return m
 }
