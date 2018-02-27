cb256f5f01bcb0c1683a717abfc5ab20abe6db85
soeyusuke <s1240231@gmail.com>
2018-02-28 00:15:40 +0900

    [ADD] messageBuild func is wrap build

--- build.go
+++ build.go
@@ -1,3 +1,19 @@
 package gostun
 
-func MessageBuild() error {}
+import "log"
+
+type transactionIDSetter struct{}
+
+// Sets *Message attribute.
+type MsgSetter interface {
+	AddTo(m *Message) error
+}
+
+var TransactionID MsgSetter = transactionIDSetter{}
+
+func MessageBuild(s ...MsgSetter) *Message {
+	m, err := Build(s...)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
