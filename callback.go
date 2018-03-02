8fbe1f0d038a3d87fafb9c32a62cadfa6d55bf98
soeyusuke <s1240231@gmail.com>
2018-03-02 23:32:40 +0900

    [ADD] callback Handle event

---
+++ callback.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+type CallbackHandle struct {
+	Cond     *sync.Cond
+	process  bool
+	callback func(e EventObject)
+}
+
+var callbackPool = sync.Pool{
+	New: func() interface{} {
+		return &CallbackHandle{
+			Cond: sync.NewCond(new(sync.Mutex)),
+		}
+	},
+}
+
+func (c *CallbackHandle) Reset() {
+	c.process = false
+	c.callback = nil
+}
+
+func (c *CallbackHandle) HandleEvent(e EventObject) {
+	if c.callback == nil {
+		log.Fatal("callback is nil")
+	}
+	c.callback(e)
+	c.Cond.L.Lock()
+	c.process = true
+	c.Cond.Broadcast()
+	c.Cond.L.Unlock()
+}
+
+func (c *CallbackHandle) Wait() {
+	c.Cond.L.Lock()
+	for !c.processed {
+		c.cond.Wait()
+	}
+	c.cond.L.Unlock()
+}
+
+func (c *Client) Call(m *Message, h Handler, rto time.Time) error {
+	f := callbackPool.Get().(*CallbackHandle)
+	f.callback = h
+
+	defer func() {
+		f.Reset()
+		callbackPool.Put(f)
+	}()
+
+	// waiting TransactionLaunch until call callback func
+	if err := c.TransactionLaunch(m, f, rto); err != nil {
+		return err
+	}
+	f.Wait()
+
+	return nil
+}
