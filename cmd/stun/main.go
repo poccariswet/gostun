458b97532f075ab2b87bbb6879327fd0a125ef42
soeyusuke <s1240231@gmail.com>
2018-03-16 17:28:22 +0900

    [FIX] had been sending message doesn't have Attribiute ( #1 )

--- cmd/stun/main.go
+++ cmd/stun/main.go
@@ -19,20 +19,14 @@ func main() {
 	}
 
 	m := gostun.MessageBuild(gostun.TransactionID, gostun.BindingRequest)
-	//	fmt.Printf("Type: %x, %x\n", m.Type.Class, m.Type.Method)
-	//	fmt.Printf("Length: %v\n", m.Length)
-	//	fmt.Printf("Magic cookie: %x\n", m.Raw[4:8])
-	//	fmt.Printf("TransactionID: %x\n", m.TransactionID)
-	//	fmt.Printf("Attributes: %v\n", m.Attributes)
-	//	fmt.Printf("Raw: %v\n", m.Raw)
 
 	f := func(event gostun.EventObject) {
 		var addr gostun.XORMappedAddr
 		if event.Err != nil {
 			log.Fatal(err)
 		}
-		if err := addr.GetXORMapped(m); err != nil {
-			log.Print(err)
+		if err := addr.GetXORMapped(event.Msg); err != nil {
+			log.Fatal(err)
 		}
 		fmt.Println(addr)
 	}
